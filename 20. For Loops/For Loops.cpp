faf90af95795dd5c929e94c15a51e9ab553c5efc
Nick <Nicholas.Bashar@gmail.com>
2025-09-06 21:32:07 -0600

    Added For Loops, and Added project number to Do While Loops

---
+++ 20. For Loops/For Loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // Loops:
+    // For              = Performs a loop a specified amount of times
+
+
+    for (int i = 10; i > 0; i--) 
+    {
+        std::cout << i << '\n';
+    }
+
+    std::cout << "HAPPY NEW YEAR!!\n";
+
+    return 0;
+}
+
