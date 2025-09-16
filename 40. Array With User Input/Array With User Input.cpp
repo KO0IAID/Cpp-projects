c2ec6645f13dc614106b9d8404159d502aeeef4b
Nick <Nicholas.Bashar@gmail.com>
2025-09-16 12:02:05 -0600

    Added Array With User Input

---
+++ 40. Array With User Input/Array With User Input.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string foods[5];
+    int size = sizeof(foods) / sizeof(foods[0]);
+    std::string temp;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
+        getline(std::cin, temp);
+
+        if (temp == "q") 
+        {
+            break;
+        }
+        else
+        {
+            foods[i] = temp;
+        }
+    }
+
+    std::cout << "\nYou like the following foods:\n";
+    // Instead of using a size, we check if the value is not empty
+    for (int i = 0; !foods[i].empty(); i++)
+    {
+        std::cout << foods[i] << '\n';
+    }
+
+    return 0;
+}
